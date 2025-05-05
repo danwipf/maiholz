057e30aae3c921bdec9bb836474f25b4be376ea5
Daniel Wipf <wipf.daniel@gmx.ch>
2025-05-05 23:18:13 +0200

    Updates

---
+++ precache-manifest.cc2175f42e75184a48b7328b68cfd819.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65eecab43b5cee3549ec543446707623",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6585c01729e6e24568f3",
+    "url": "/static/css/main.eb4804f8.chunk.css"
+  },
+  {
+    "revision": "956cb6245c2565f6fffc",
+    "url": "/static/js/2.072cde9e.chunk.js"
+  },
+  {
+    "revision": "1aef9e0d481a6908c30509eb88500877",
+    "url": "/static/js/2.072cde9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dc51c043a54ee298ea5",
+    "url": "/static/js/3.843cbd7e.chunk.js"
+  },
+  {
+    "revision": "6585c01729e6e24568f3",
+    "url": "/static/js/main.81dfc4c9.chunk.js"
+  },
+  {
+    "revision": "bf5255c781c75ed262ff",
+    "url": "/static/js/runtime-main.79de12a1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4a74518207604659c341814e31de1089.js"
+  "/precache-manifest.cc2175f42e75184a48b7328b68cfd819.js"
 );
 
 self.addEventListener('message', (event) => {

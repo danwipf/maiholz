01b08573bcfe07b3e5b7c299ad8e73a56579341b
Daniel Wipf <wipf.daniel@gmx.ch>
2025-05-05 16:10:18 +0200

    Updates

---
+++ precache-manifest.494e763c9e17f1efa972e4cde832344d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abc9cd258f6ce37cc323bf32d359d07b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09d9dfdb8d7a26defd7c",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a2d17de9c49c2599fbbe",
+    "url": "/static/js/2.d2752a5d.chunk.js"
+  },
+  {
+    "revision": "0e70cb57d9eff2ad4cf363da0809cb30",
+    "url": "/static/js/2.d2752a5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3ebc11a5c8999bcb529",
+    "url": "/static/js/3.e3af7e68.chunk.js"
+  },
+  {
+    "revision": "09d9dfdb8d7a26defd7c",
+    "url": "/static/js/main.c668e839.chunk.js"
+  },
+  {
+    "revision": "51d4a61b237ed18668a3",
+    "url": "/static/js/runtime-main.1f270bc2.js"
+  }
+]);
